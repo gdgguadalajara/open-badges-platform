f5de420392c9119cdfe98dfc8523628914d57df8
ushieru <uzielcocolan@gmail.com>
2026-01-27 16:22:30 -0600

    Feat(MemberRole): add EDITOR role for managing BadgeClass drafts

--- src/main/java/com/gdgguadalajara/membership/model/MemberRole.java
+++ src/main/java/com/gdgguadalajara/membership/model/MemberRole.java
@@ -3,5 +3,4 @@ package com.gdgguadalajara.membership.model;
 public enum MemberRole {
     OWNER, // Puede gestionar otros admins
     ADMIN, // Puede crear BadgeClasses y emitir Assertions
-    EDITOR // Solo puede ver y editar borradores de BadgeClasses
 }
