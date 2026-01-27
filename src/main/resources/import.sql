c4205620c353cefeeff083de2296fe5bb66ab1bb
ushieru <uzielcocolan@gmail.com>
2026-01-27 16:24:46 -0600

    Feat(ImportSQL): add initial data for Account, Issuer, and IssuerMember

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,15 @@
 INSERT INTO Account(createdAt, email, fullName, isSuperAdmin, isVerified, password, id) 
     VALUES(0, 'admin@gdgguadalajara.com', 'OBP Admin', true, true, '$2a$10$bLcqAjUZApBkS6LXAGe2KeDPJCYvOOhgigAvTef797q8mSDiEE2J2', X'96cf44a2ce0c4aeba61df7e5892daada');
+
+INSERT INTO Issuer (createdAt, description, email, jsonPayload, logoUrl, name, url, id) 
+    VALUES(0, 
+    'Comunidad oficial de desarrolladores en Guadalajara apoyada por Google. Organizamos eventos gratuitos, charlas técnicas y talleres sobre desarrollo de software, nube e IA para fortalecer el talento tecnológico en la región.',
+    'contacto@gdgguadalajara.com',
+    '{"@context": "https://w3id.org/openbadges/v2","type": "Issuer","id": "http://127.0.0.1:8080/api/v2/issuers/3b73cde4-b3ee-442b-8b5b-3ea6dde14d9d","name": "Google Developer Group Guadalajara","url": "https://gdgguadalajara.com/","email": "contacto@gdgguadalajara.com","description": "Comunidad oficial de desarrolladores en Guadalajara apoyada por Google. Organizamos eventos gratuitos, charlas técnicas y talleres sobre desarrollo de software, nube e IA para fortalecer el talento tecnológico en la región.","image": "https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-goog/contentbuilder/GDG-Lockup-1Line-Black_vMWBFT9.svg","revocationList": "http://127.0.0.1:8080/api/v2/issuers/3b73cde4-b3ee-442b-8b5b-3ea6dde14d9d/revocations"}',
+    'https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-goog/contentbuilder/GDG-Lockup-1Line-Black_vMWBFT9.svg',
+    'Google Developer Group Guadalajara',
+    'https://gdgguadalajara.com',
+    X'3b73cde4b3ee442b8b5b3ea6dde14d9d');
+
+INSERT INTO IssuerMember(account_id, createdAt, issuer_id, role, id) 
+    VALUES(X'96cf44a2ce0c4aeba61df7e5892daada', 0, X'3b73cde4b3ee442b8b5b3ea6dde14d9d', 'OWNER', X'c43856de4c0d4b7ea4009dcb775d2a79');
