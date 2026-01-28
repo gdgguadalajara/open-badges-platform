b8712b2b863aa5eb612da46a3c3d1ab2b524c945
ushieru <uzielcocolan@gmail.com>
2026-01-28 10:55:21 -0600

    Feat(Authentication): implement me endpoint and add MeResponse DTO

--- src/main/java/com/gdgguadalajara/authentication/AuthenticationResource.java
+++ src/main/java/com/gdgguadalajara/authentication/AuthenticationResource.java
@@ -1,28 +1,47 @@
 package com.gdgguadalajara.authentication;
 
-import com.gdgguadalajara.account.model.Account;
-import com.gdgguadalajara.common.model.DomainException;
+import java.net.URI;
 
+import com.gdgguadalajara.authentication.application.GetCurrentSession;
+import com.gdgguadalajara.authentication.model.dto.MeResponse;
+import com.gdgguadalajara.membership.model.IssuerMember;
+
+import io.quarkus.oidc.OidcSession;
 import io.quarkus.security.Authenticated;
-import io.quarkus.security.identity.SecurityIdentity;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 import lombok.AllArgsConstructor;
 
 @Path("/api/auth")
 @AllArgsConstructor
 public class AuthenticationResource {
 
-    private final SecurityIdentity identity;
+    private final OidcSession oidcSession;
+    private final GetCurrentSession getCurrentSession;
 
     @GET
     @Path("/me")
     @Authenticated
-    public Account login() {
-        var email = identity.getPrincipal().getName();
-        var account = Account.<Account>find("email", email).firstResult();
-        if (account == null)
-            throw DomainException.badRequest("No account found for the authenticated user");
-        return account;
+    public MeResponse me() {
+        var account = getCurrentSession.run();
+        var memberships = IssuerMember.<IssuerMember>list("account", account);
+        return new MeResponse(account, memberships);
+    }
+
+    @GET
+    @Path("/login")
+    @Authenticated
+    public Response login() {
+        return Response.seeOther(URI.create("/login")).build();
+    }
+
+    @GET
+    @Path("/logout")
+    @Authenticated
+    public Uni<Response> logout() {
+        return oidcSession.logout()
+                .onItem().transform(v -> Response.seeOther(URI.create("/")).build());
     }
 }
---
+++ src/main/java/com/gdgguadalajara/authentication/model/dto/MeResponse.java
@@ -0,0 +1,12 @@
+package com.gdgguadalajara.authentication.model.dto;
+
+import java.util.List;
+
+import com.gdgguadalajara.account.model.Account;
+import com.gdgguadalajara.membership.model.IssuerMember;
+
+public record MeResponse(
+        Account account,
+        List<IssuerMember> memberships) {
+
+}
