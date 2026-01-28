cf29f105aa29f873e5ccb75d3d3322b12d9a1fca
ushieru <uzielcocolan@gmail.com>
2026-01-28 11:05:13 -0600

    Feat(Pagination): add PaginationRequestParams DTO for pagination support

---
+++ src/main/java/com/gdgguadalajara/common/model/dto/PaginationRequestParams.java
@@ -0,0 +1,19 @@
+package com.gdgguadalajara.common.model.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.QueryParam;
+
+public class PaginationRequestParams {
+    @QueryParam("page")
+    @DefaultValue("1")
+    @Positive
+    public Integer page;
+
+    @QueryParam("size")
+    @DefaultValue("10")
+    @Positive
+    @Max(100)
+    public Integer size;
+}
