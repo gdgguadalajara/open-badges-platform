c999d832ecff6de5dceae7fb976fc363828dd39f
ushieru <uzielcocolan@gmail.com>
2026-01-31 12:43:09 -0600

    Feat(UpdateAccount): add domain configuration and enhance assertion HTML payload processing

--- src/main/java/com/gdgguadalajara/account/application/UpdateAccount.java
+++ src/main/java/com/gdgguadalajara/account/application/UpdateAccount.java
@@ -1,7 +1,11 @@
 package com.gdgguadalajara.account.application;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import com.gdgguadalajara.account.model.Account;
 import com.gdgguadalajara.account.model.dto.UpdateAccountRequest;
+import com.gdgguadalajara.assertion.application.CreateAssertion;
+import com.gdgguadalajara.assertion.model.Assertion;
 import com.gdgguadalajara.authentication.application.GetCurrentSession;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -14,11 +18,27 @@ public class UpdateAccount {
 
     private final GetCurrentSession getCurrentSession;
 
+    @ConfigProperty(name = "com.gdgguadalajara.open-badges-platform.domain")
+    public String domain;
+
     @Transactional
     public Account run(UpdateAccountRequest request) {
         var account = getCurrentSession.run();
         account.fullName = request.name();
         account.persistAndFlush();
+        var assertions = Assertion.<Assertion>list("account = ?1", account);
+        for (var assertion : assertions) {
+            assertion.htmlPayload = CreateAssertion.Templates.htmlPayload(domain, assertion).render()
+                    .replaceAll("(?s)", "")
+                    .replaceAll("(?s)\\s+", " ")
+                    .replaceAll("> <", "><")
+                    .replaceAll("\\s+\\{", "{")
+                    .replaceAll("\\{\\s+", "{")
+                    .replaceAll(";\\s+", ";")
+                    .trim();
+            assertion.persist();
+        }
+        Assertion.flush();
         return account;
     }
 }
