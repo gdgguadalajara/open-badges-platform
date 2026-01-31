3c66d3fd2b99da4e5aafac5364a0bccc006a1df0
ushieru <uzielcocolan@gmail.com>
2026-01-31 12:57:45 -0600

    Feat(GetCurrentSession): change scope to RequestScoped and optimize account retrieval with caching

--- src/main/java/com/gdgguadalajara/authentication/application/GetCurrentSession.java
+++ src/main/java/com/gdgguadalajara/authentication/application/GetCurrentSession.java
@@ -4,20 +4,24 @@ import com.gdgguadalajara.account.model.Account;
 import com.gdgguadalajara.common.model.DomainException;
 
 import io.quarkus.security.identity.SecurityIdentity;
-import jakarta.enterprise.context.ApplicationScoped;
-import lombok.AllArgsConstructor;
+import jakarta.enterprise.context.RequestScoped;
+import lombok.RequiredArgsConstructor;
 
-@ApplicationScoped
-@AllArgsConstructor
+@RequestScoped
+@RequiredArgsConstructor
 public class GetCurrentSession {
 
     private final SecurityIdentity identity;
+    private Account cachedAccount;
 
     public Account run() {
+        if (cachedAccount != null)
+            return cachedAccount;
         var email = identity.getPrincipal().getName();
         var account = Account.<Account>find("email", email).firstResult();
         if (account == null)
             throw DomainException.notFound("Cuenta no encontrada");
+        cachedAccount = account;
         return account;
     }
 }
